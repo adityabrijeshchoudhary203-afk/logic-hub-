1afa3b09195319391c0abab91483560ef0fe1386
adityabrijeshchoudhary203-afk <adityabrijeshchoudhary203@gmail.com>
2026-08-19 20:33:17 +0530

    harshad number

---
+++ hrshad number5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,sum,d,copy;
+cout<<"enter the number";
+cin>>n;
+copy=0;
+sum=0;
+while(n!=1){
+d=n%10;
+sum=sum+d;
+n=n/10;}
+{
+if (copy%sum==0) =
+cout<<"harshad numberr";
+else 
+cout<<"not a harshad number";
+
+}
+return 0;
+}
