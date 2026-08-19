b68c650d9e3979a2d627821ebb216c2994c0d0cf
adityabrijeshchoudhary203-afk <adityabrijeshchoudhary203@gmail.com>
2026-08-19 20:40:03 +0530

    sum pro number

---
+++ sumprorange1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,start,end,sum,j,i,dig;
+	cout<<"enter start and end range";
+	cin>>start>>end;
+	sum=0;
+	pro=1;
+	for(i=start;i<=end;i++)
+	{dig=j%10;
+	sum=sum+dig;
+	pro=pro+dig;
+	}
+}
+cout<<"summ pro number is"<<sum;
+return 0;
+}
+
